b397cb2ffa4f534122533b9874a12fdf2b661563
Alexa <alexakearns@outlook.com>
2020-05-11 22:30:38 +0100

    plane.land changes flight status - Red

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,7 +2,14 @@ require 'plane'
 
 describe Plane do
 
+
+
   it 'plane defaults to in_flight' do
     expect(subject.flying?).to be true
   end
+
+  it 'plane landing changes flight status to not in flight' do
+    subject.land
+    expect(subject.flying?).to be false
+  end  
 end
