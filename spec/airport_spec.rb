117137c242492a3f350af13a4a789a6d259e5ba9
Alexa <alexakearns@outlook.com>
2020-05-11 21:26:16 +0100

    mock plane for testing plane object landing

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,8 +2,13 @@ require 'airport'
 
 describe Airport do
 
+  let(:boeing) {Plane.new}
 
   it 'plane in as argument lands at airport' do
     expect(subject.land("plane")).to eq "plane"
   end
+
+  it 'returns plane object in #land at airport' do
+    expect(subject.land(boeing)).to be_an_instance_of(Plane)
+end
 end
