4b43cf27f06f8b4fbc1c0c9b44c05a648953f69f
Alexa <alexakearns@outlook.com>
2020-05-11 21:31:03 +0100

    plane can take off from airport - Red

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,4 +4,5 @@ class Airport
     plane
   end
 
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,13 +4,20 @@ describe Airport do
 
   let(:boeing) {Plane.new}
 
-  it 'plane in as argument lands at airport' do
-    expect(subject.land("plane")).to eq "plane"
+  context '#land' do
+    it 'plane in as argument lands at airport' do
+      expect(subject.land("plane")).to eq "plane"
+    end
+
+    it 'returns plane object in #land at airport' do
+      expect(subject.land(boeing)).to be_an_instance_of(Plane)
+    end
   end
 
-  it 'returns plane object in #land at airport' do
-    expect(subject.land(boeing)).to be_an_instance_of(Plane)
-  end
-  
+  context '#take off' do
+    it 'plane can #take off from airport' do
+      expect(subject.take_off(boeing)).to eq boeing
+    end
+  end 
   
 end
