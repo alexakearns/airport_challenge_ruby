d5bc3074a9c78cecca8f063f84591dcce1ca8d55
Alexa <alexakearns@outlook.com>
2020-06-19 18:13:58 +0100

    Add Weather class

---
+++ spec/weather_spec.rb
@@ -0,0 +1,17 @@
+require 'weather'
+
+describe Weather do
+
+
+end
+  # context '#stormy?' do
+  #   it 'stormy weather returns true' do
+  #     stormy_weather
+  #     expect(subject.stormy?).to be true
+  #   end
+
+  #   it 'good weather returns false' do
+  #     good_weather
+  #     expect(subject.stormy?).to be false
+  #   end
+  # end
