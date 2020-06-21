929611809a395312648ff7f975473bd9b32abd36
Alexa <alexakearns@outlook.com>
2020-06-21 09:58:52 +0100

    weather class stormy weather return true - Red

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,17 +1,10 @@
 require 'weather'
 
 describe Weather do
+  let(:stormy_weather) { allow(subject).to receive(:stormy?).and_return true }
 
-
+  it 'stormy weather returns true' do
+    stormy_weather
+    expect(subject.stormy?).to be true
+  end
 end
-  # context '#stormy?' do
-  #   it 'stormy weather returns true' do
-  #     stormy_weather
-  #     expect(subject.stormy?).to be true
-  #   end
-
-  #   it 'good weather returns false' do
-  #     good_weather
-  #     expect(subject.stormy?).to be false
-  #   end
-  # end
