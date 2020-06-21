82bfbbd33017258a2a98e03cda08c124905c98a1
Alexa <alexakearns@outlook.com>
2020-06-21 10:00:04 +0100

    good weather to return false - Green

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,9 +2,15 @@ require 'weather'
 
 describe Weather do
   let(:stormy_weather) { allow(subject).to receive(:stormy?).and_return true }
+  let(:good_weather) { allow(subject).to receive(:stormy?).and_return false }
 
   it 'stormy weather returns true' do
     stormy_weather
     expect(subject.stormy?).to be true
+  end
+
+  it 'good weather returns false' do
+    good_weather
+    expect(subject.stormy?).to be false
   end
 end
